58de7fece2f551a762a5423bf4e617335aaeba8d
Takayuki Shimada <taka@tsmd.jp>
2014-11-26 20:48:29 +0900

    PHPファイルはスペース4つが主流みたいなのでそうする

--- htdocs/api/v1/index.php
+++ htdocs/api/v1/index.php
@@ -12,18 +12,18 @@ usleep(rand(300, 3000) * 1000);
 
 $contents = '';
 if (file_exists($file1)) {
-	$contents = file_get_contents($file1);
+    $contents = file_get_contents($file1);
 } else if (file_exists($file2)) {
-	$contents = file_get_contents($file2);
+    $contents = file_get_contents($file2);
 } else {
-	$contents = '{ "error": "File not found.", "files": [ "' . $file1 . '", "' . $file2 . '" ] }';
+    $contents = '{ "error": "File not found.", "files": [ "' . $file1 . '", "' . $file2 . '" ] }';
 }
 
 function toJsonString($str) {
-	$before = array('/\\\\/',   '/\\r/', '/\\n/', '/\\t/', '/"/');
-	$after = array( '\\\\\\\\', '\\\\r', '\\\\n', '\\\\t', '\\\\"');
-	$str = preg_replace($before, $after, $str);
-	return $str;
+    $before = array('/\\\\/',   '/\\r/', '/\\n/', '/\\t/', '/"/');
+    $after = array( '\\\\\\\\', '\\\\r', '\\\\n', '\\\\t', '\\\\"');
+    $str = preg_replace($before, $after, $str);
+    return $str;
 }
 
 // <!--#include virtual=\"...\"--> で囲まれた部分をファイルを取ってきてむりやり置換する
@@ -31,10 +31,10 @@ $fileBase = '../..';
 $pattern = '/<!--\\s*#include\\s+virtual=\\\\"(.+)\\\\"\\s*-->/';
 $i = 0;
 while (preg_match($pattern, $contents, $match)) {
-	$escaped = toJsonString(file_get_contents($fileBase . $match[1]));
-	$contents = str_replace($match[0], $escaped, $contents);
-	$i ++;
-	if ($i > 100) { echo 'break'; break; }
+    $escaped = toJsonString(file_get_contents($fileBase . $match[1]));
+    $contents = str_replace($match[0], $escaped, $contents);
+    $i ++;
+    if ($i > 100) { echo 'break'; break; }
 }
 
 print($contents);
